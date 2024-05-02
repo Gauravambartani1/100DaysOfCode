b9db4d1f285be902a1824c1039ce09aca219b08d
Gauravambartani1 <gauravambartani55@gmail.com>
2024-05-02 17:25:13 +0530

    Day 3

---
+++ Day 3/treasureIsland.py
@@ -0,0 +1,7 @@
+print("Welcome to treasure island")
+c1=input("Where do you wanna go? left or right? ")
+c1 = c1.lower()
+if(c1 == "right"):
+    print("Game over, a fire breathing dragon swallowed you")
+elif (c1=="left"):
+    print("You won!") 
